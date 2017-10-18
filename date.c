229203ba2b738f3f9b338c807ea0f78a98e6eeb4
tsbxmw <1050636648@qq.com>
2017-10-18 10:13:21 +0800

    format date.c

--- date.c
+++ date.c
@@ -175,46 +175,48 @@ void print_number(char *date){
                              if(x==6)
                                printf("\033[1;38m%c",time[i][j]);
                                
-                            
                        }
                }
                printf("\n");
         }
 }
+
 int main(){
-while(1){
-printf("\033[1;32m*****   **   ***   ***   *****    \n");
-printf("\033[31m  *          *  * *  *   *           **\n");
-printf("\033[1;34m  *     **   *   *   *   *****    \n");
-printf("\033[1;33m  *     **   *   *   *   *           ** \n");
-printf("\033[1;37m  *     **   *   *   *   *****    \n\n\n");
-printf("\033\[1;32m\n");
-FILE *stream;
-char buf[50];
-stream = popen("date","r");
-fread(buf,sizeof(char),sizeof(buf),stream);
-char *tmp;
-int i = 0;
-tmp = strtok(buf," ");
-while(tmp!=NULL&&i<4){
-    tmp = strtok(NULL," ");
-    //at ubuntu 16.04 
-    /*
-        if(i==2)
-           i++;
-    */
-    if(i==3){ 
-        i = 4;
+
+    while(1){
+        printf("\033[1;32m*****   **   ***   ***   *****    \n");
+        printf("\033[31m  *          *  * *  *   *           **\n");
+        printf("\033[1;34m  *     **   *   *   *   *****    \n");
+        printf("\033[1;33m  *     **   *   *   *   *           ** \n");
+        printf("\033[1;37m  *     **   *   *   *   *****    \n\n\n");
+        printf("\033\[1;32m\n");
+
+        FILE *stream;
+        char buf[50];
+        stream = popen("date","r");
+        fread(buf,sizeof(char),sizeof(buf),stream);
+        char *tmp;
+        int i = 0;
+        tmp = strtok(buf," ");
+        while(tmp!=NULL&&i<4){
+            tmp = strtok(NULL," ");
+            //at ubuntu 16.04 
+            /*
+                if(i==2)
+                   i++;
+            */
+            if(i==3){ 
+                i = 4;
+            }
+            i++;
+        }
+        pclose(stream);
+        print_number(tmp);
+        //system("sleep 1");
+        usleep(100000);
+        system("clear");
     }
-    i++;
-}
-pclose(stream);
-print_number(tmp);
-//system("sleep 1");
-usleep(100000);
-system("clear");
-}
-return 0;
+    return 0;
 }
 
 
