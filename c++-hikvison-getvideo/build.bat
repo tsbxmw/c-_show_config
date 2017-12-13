d96468e976392a6042ddcefe036e2f100a98f06d
tsbxmw <1050636648@qq.com>
2017-12-13 16:38:06 +0800

    rebuild the get video

--- c++-hikvison-getvideo/build.bat
+++ c++-hikvison-getvideo/build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo Loading the vs2010 environment...
-call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86\
+call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
 
 echo Set the environment variable...
 set SCRIPT_ROOT=%~dp0
@@ -9,7 +9,7 @@ set SDK_ROOT=%SCRIPT_ROOT%
 set BUILD_ROOT=%SDK_ROOT%
 
 
-echo Building slamware sdk test...
+echo Building get video from hikivision test...
 
 set WORKSPACE_ROOT=%SDK_ROOT%
 
