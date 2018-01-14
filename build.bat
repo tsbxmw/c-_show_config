068a0d6ee0f66cfe15c216c22fe91e6abbcc9373
tsbxmw <1050636648@qq.com>
2018-01-14 10:59:36 +0800

    bug fix : dir changed

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
-msbuild "C:\projects\c-show-config\c++-getkeydown-background\getkeydown.sln?" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+msbuild "C:\projects\c-show-config\c++-getkeydown-background\getkeydown.sln" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 echo ------------------------------------------------------------------------------------------------
-msbuild "C:\projects\c-show_config\c++-GetH264FromRtsp\GetH264FromRtsp.vcxproj" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+msbuild "C:\projects\c-show-config\c++-GetH264FromRtsp\GetH264FromRtsp.vcxproj" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 
 echo ------------------------------------------------------------------------------------------------
-msbuild "C:\projects\c-show_config\c++-getkeydown-background\getkeydown.vcxproj" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+msbuild "C:\projects\c-show-config\c++-getkeydown-background\getkeydown.vcxproj" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 
 echo ------------------------------------------------------------------------------------------------
-msbuild "C:\projects\c-show_config\c++-hikvison-getvideo\test.vcxproj" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+msbuild "C:\projects\c-show-config\c++-hikvison-getvideo\test.vcxproj" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 
